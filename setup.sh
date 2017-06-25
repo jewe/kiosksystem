a606a12fa3846b7783682ab5f951393ac475a836
Jens Weber <newsletter@jewe.net>
2017-06-25 23:55:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 
+# update aller pakete
 sudo apt-get update
+sudo apt-get dist-upgrade
 
+# uninstall unneeded applications
+sudo apt-get remove -y firefox
+sudo apt-get remove -y rhythmbox
+sudo apt-get remove -y samba-common
+sudo apt-get remove -y seahorse
+sudo apt-get remove -y thunderbird
+sudo apt-get remove -y ubuntu-mate-libreoffice-draw-icons
+sudo apt-get remove -y hexchat
+sudo apt-get remove -y simple-scan
+sudo apt-get remove -y pidgin
+sudo apt-get remove -y transmission
+sudo apt-get remove -y brasero
+sudo apt-get remove -y cheese
+sudo apt-get remove -y rhythmbox
 
 # hide mouse
-sudo apt-get install unclutter
+sudo apt-get install -y unclutter
