8862b27dd2735bd05301aceeca342fe89a90994e
Jens Weber <newsletter@jewe.net>
2017-06-26 00:09:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ sudo apt-get remove -y samba-common
 sudo apt-get remove -y seahorse
 sudo apt-get remove -y thunderbird
 sudo apt-get remove -y ubuntu-mate-libreoffice-draw-icons
+sudo apt-get remove -y libreoffice-core
 sudo apt-get remove -y hexchat
 sudo apt-get remove -y simple-scan
 sudo apt-get remove -y pidgin
@@ -20,3 +21,20 @@ sudo apt-get remove -y rhythmbox
 
 # hide mouse
 sudo apt-get install -y unclutter
+
+
+sudo apt-get install -y vim
+sudo apt-get install -y curl
+sudo apt-get install -y build-essential
+sudo apt-get install -y dropbear
+sudo apt-get install -y openssh-sftp-server
+sudo apt-get install -y zram-config
+sudo apt-get install -y nano
+#sudo apt-get install -y 
+#sudo apt-get install -y 
+
+# nodejs
+
+# platz machen
+sudo apt-get clean
+sudo apt-get autoremove
