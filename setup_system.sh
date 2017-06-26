6c2b9bbb7fa51ee29ac3b5baf632be01708f31ca
Jens Weber <newsletter@jewe.net>
2017-06-26 15:27:53 +0200

    Update setup_system.sh

--- setup_system.sh
+++ setup_system.sh
@@ -2,20 +2,11 @@
 # UNINSTALL unneeded applications
 ## check/update with
 ## apt list --installed
-sudo apt-get remove -y firefox
-sudo apt-get remove -y rhythmbox
-sudo apt-get remove -y samba-common
-sudo apt-get remove -y seahorse
-sudo apt-get remove -y thunderbird
+sudo apt-get remove -y firefox rhythmbox samba-common seahorse thunderbird
 sudo apt-get remove -y ubuntu-mate-libreoffice-draw-icons
 sudo apt-get remove -y libreoffice-core
-sudo apt-get remove -y hexchat
-sudo apt-get remove -y simple-scan
-sudo apt-get remove -y pidgin
-sudo apt-get remove -y transmission
-sudo apt-get remove -y brasero
-sudo apt-get remove -y cheese
-sudo apt-get remove -y rhythmbox
+sudo apt-get remove -y hexchat simple-scan pidgin transmission brasero cheese
+
 
 # remove unused locals
 export DISPLAY=:0.0
