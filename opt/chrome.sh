cc61bd5af114a4c0706bfbbc14b55586a9851e7e
Jens Weber <newsletter@jewe.net>
2017-06-26 14:04:04 +0200

    Update chrome.sh

--- opt/chrome.sh
+++ opt/chrome.sh
@@ -7,5 +7,5 @@ xset s off
 
 
 while true; do rm -rf ~/.{config,cache}/google-chrome/
-  chromium-browser --kiosk --no-first-run --incognito --no-default-browser-check --disable-translate 'http://localhost'
+  chromium-browser --kiosk --no-first-run --incognito --no-default-browser-check --disable-translate --disk-cache-dir=/tmp/cache 'http://localhost'
 done
