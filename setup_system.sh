f2b64dc57f2a4f7e17288249be7ab493ba1573f7
Jens Weber <newsletter@jewe.net>
2017-06-27 12:42:59 +0200

    Update setup_system.sh

--- setup_system.sh
+++ setup_system.sh
@@ -62,7 +62,7 @@ gsettings set org.mate.screensaver lock-enabled false
 sudo rm -Rf /usr/share/backgrounds/*
 sudo mv ./WW_Wallpaper.png /usr/share/backgrounds/
 sudo mv ./WW_Wallpaper_HD.png /usr/share/backgrounds/
-gsettings set org.mate.background picture-filename /usr/share/backgrounds/WW_Wallpaper.png
+gsettings set org.mate.background picture-filename /usr/share/backgrounds/WW_Wallpaper_HD.png
 
 gsettings set org.mate.background primary-color '#555555'
 gsettings set org.mate.background secondary-color '#444444'
@@ -71,7 +71,7 @@ gsettings set org.mate.background show-desktop-icons false
 gsettings set org.mate.background picture-options 'centered'
 
 # what's the difference here?
-gsettings set org.gnome.desktop.background picture-uri /usr/share/backgrounds/WW_Wallpaper.png
+gsettings set org.gnome.desktop.background picture-uri /usr/share/backgrounds/WW_Wallpaper_HD.png
 
 gsettings set org.gnome.desktop.background primary-color '#555555'
 gsettings set org.gnome.desktop.background secondary-color '#444444'
@@ -96,38 +96,7 @@ dconf write /org/mate/panel/toplevels/top/auto-hide true
 # hide bottom panel
 dconf write /org/mate/panel/toplevels/bottom/auto-hide true
 
-# config all elements 
-# dconf write /org/mate/panel/general/object-id-list [...]
 
-# check this:
-#org.mate.NotificationDaemon popup-location 'top_right'
-#org.mate.gnome-main-menu.lock-down
-#org.gnome.settings-daemon.plugins.power
-
-#org.mate.panel.menubar icon-name 'start-here'
-#org.mate.panel.menubar show-icon true
-#org.mate.panel.menubar show-places true
-#org.mate.panel.menubar show-desktop true
-#org.mate.panel.menubar show-applications true
-
-
-#org.gnome.desktop.lockdown disable-command-line false
-#org.gnome.desktop.lockdown disable-application-handlers false
-#org.gnome.desktop.lockdown disable-user-switching false
-#org.gnome.desktop.lockdown user-administration-disabled false
-#org.gnome.desktop.lockdown disable-printing false
-#org.gnome.desktop.lockdown disable-log-out false
-#org.gnome.desktop.lockdown disable-lock-screen false
-#org.gnome.desktop.lockdown disable-print-setup false
-#org.gnome.desktop.lockdown disable-save-to-disk false
-
-
-# check and remove unneeded services
-# systemctl list-units
-
-# bluetooth off
-# TODO make persistent
-sudo rfkill block bluetooth
 
 # readonly filesystem
 # https://wiki.ubuntuusers.de/Nur-Lesen_Root-Dateisystem/
