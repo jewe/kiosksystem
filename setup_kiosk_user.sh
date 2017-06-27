327be1fbc06355d9297b24a5336f8aaeb77209a1
Jens Weber <newsletter@jewe.net>
2017-06-27 23:13:04 +0200

    Update setup_kiosk_user.sh

--- setup_kiosk_user.sh
+++ setup_kiosk_user.sh
@@ -16,8 +16,8 @@ gsettings set org.mate.screensaver lock-enabled false
 
 # set background
 sudo rm -Rf /usr/share/backgrounds/*
-sudo mv ./backgrounds/WW_Wallpaper.png /usr/share/backgrounds/
-sudo mv ./backgrounds/WW_Wallpaper_HD.png /usr/share/backgrounds/
+sudo cp ./backgrounds/WW_Wallpaper.png /usr/share/backgrounds/
+sudo cp ./backgrounds/WW_Wallpaper_HD.png /usr/share/backgrounds/
 gsettings set org.mate.background picture-filename /usr/share/backgrounds/WW_Wallpaper.png
 
 gsettings set org.mate.background primary-color '#555555'
