858ccacf6ae3432bbb6491cbde6a8dac86e4c2eb
Jens Weber <newsletter@jewe.net>
2017-06-27 11:30:34 +0200

    Update chrome.sh

--- opt/chrome.sh
+++ opt/chrome.sh
@@ -8,6 +8,11 @@ xset -dpms
 xset s off
 
 
+# If Chromium crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
+#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
+#sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
+
+
 while true; do rm -rf ~/.{config,cache}/google-chrome/
   chromium-browser --kiosk --no-first-run --incognito --no-default-browser-check --disable-translate --disk-cache-dir=/tmp/cache 'http://localhost'
 done
