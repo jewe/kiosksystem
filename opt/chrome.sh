371774519ef03247e5dbd9a789d172a8a0d71a6c
Jens Weber <newsletter@jewe.net>
2017-06-28 13:54:33 +0200

    Update chrome.sh

--- opt/chrome.sh
+++ opt/chrome.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+SERVICE="/opt/service.browser"
+
+case "$1" in enabled|disabled) echo "$1" >"$SERVICE"; exit ;;  esac
+
+exit_if_disabled() {
+  read -r SWITCH <"$SERVICE"  
+  test "$SWITCH" = 'enabled' || exit
+}
+
+exit_if_disabled
+
+log() {
+  logger -s -- "$0: $*"
+  echo "$( date ): $0: $*" >>"$SERVICE.log" 
+}
+
+### here it starts
+
 export DISPLAY=:0.0
 
 # ?
@@ -12,7 +30,8 @@ xset s off
 #sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
 #sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
 
-
 while true; do rm -rf ~/.{config,cache}/google-chrome/
+  log "gestartet: $SERVICE"
   chromium-browser --kiosk --no-first-run --incognito --no-default-browser-check --disable-translate --disk-cache-dir=/tmp/cache 'http://localhost'
+  exit_if_disabled
 done
