971ead7beba29b92446cc5680917fd5722c0c233
Jens Weber <newsletter@jewe.net>
2017-06-29 11:44:31 +0200

    Create install_processing.sh

---
+++ install_processing.sh
@@ -0,0 +1,14 @@
+printf "\n------------\n"
+echo "INSTALL PROCESSING 3.3.5"
+
+export DISPLAY=:0.0
+
+
+sudo wget http://download.processing.org/processing-3.3.5-linux64.tgz
+sudo tar -xzf processing-3.3.5-linux64.tgz -C /opt/
+
+sudo su -c "ln -s /opt/processing-3.3.5/processing /usr/local/bin/processing"
+
+# see http://www.artsnova.com/processing/installing-processing-ubuntu-linux-tutorial.html
+# and https://forum.processing.org/two/discussion/1523/shortcut-for-processing-in-ubuntu-13-10-and-unity
+
