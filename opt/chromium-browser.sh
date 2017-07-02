330aa1ddac3a9b42859cd4987927ea45e3b74162
Jens Weber <newsletter@jewe.net>
2017-07-02 19:45:02 +0200

    Update chromium-browser.sh

--- opt/chromium-browser.sh
+++ opt/chromium-browser.sh
@@ -9,32 +9,16 @@ SERVICE=${SERVICE%.*}
 
 kiosklog "$SERVICE" "$SERVICE script started $0"
 
-#case "$1" in enabled|disabled) echo "$1" >"/opt/kiosk/services/$SERVICE"; exit ;;  esac
-
 exit_if_disabled "$SERVICE"
-
-#exit_if_disabled() {
-#  read -r SWITCH <"$SERVICE"  
-#  test "$SWITCH" = 'enabled' || exit
-#}
-
-#exit_if_disabled
-
-#log() {
-#  logger -s -- "$0: $*"
-#  echo "$( date ): $0: $*" >>"$SERVICE.log" 
-#}
-
 
 # If Chromium crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
 #sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/kiosk/.config/chromium/Default/Preferences
 #sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/kiosk/.config/chromium/Default/Preferences
 
-while true; do #rm -rf ~/.{config,cache}/google-chrome/
-  #log "gestartet: $SERVICE"
-  #chromium-browser --kiosk --no-first-run --incognito --no-default-browser-check --disable-translate --disk-cache-dir=/tmp/cache 'http://localhost'
-  #exit_if_disabled
-  
+while true; do 
+  #rm -rf ~/.{config,cache}/google-chrome/
   kiosklog "$SERVICE" "$SERVICE started"
-  exit 0
+  chromium-browser --kiosk --no-first-run --incognito --no-default-browser-check --disable-translate --disk-cache-dir=/tmp/cache 'http://localhost'
+  kiosklog "$SERVICE" "$SERVICE crashed"
+  exit_if_disabled
 done
