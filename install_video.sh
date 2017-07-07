49f8e43779cb6f3e963b0ca37b748637360b9b56
jewe <software@jewe.net>
2017-07-07 12:54:48 +0200

    ...

---
+++ install_video.sh
@@ -0,0 +1,21 @@
+printf "\n------------\n"
+echo "INSTALL VLC Video"
+
+export DISPLAY=:0.0
+
+
+# copy start-script
+sudo cp ./opt/video.sh /opt/
+sudo chmod +x /opt/video.sh
+
+. /opt/tmp/kiosksystem/opt/global_functions
+kioskctl enable video
+
+# autostart 
+sudo cp ./autostart/video.sh.desktop /home/kiosk/.config/autostart/
+sudo chown kiosk /home/kiosk/.config/autostart/video.sh.desktop
+sudo chmod +x /home/kiosk/.config/autostart/video.sh.desktop
+
+# sample images
+sudo cp -R sample_Images/*.mov /home/kiosk/Pictures/
+
