add38a554386649fc6b9ee42f80cb7cd7e7cdf9d
jewe <software@jewe.net>
2019-01-24 14:42:35 +0100

    ...

--- install_arduino.sh
+++ install_arduino.sh
@@ -6,9 +6,9 @@ echo "INSTALL ARDUINO 1.8.8"
 export DISPLAY=:0.0
 
 
-sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.8-linux64.tar.xz
-sudo tar -xzf arduino* -C /opt/
-sudo rm arduino*.tgz
+sudo wget https://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
+sudo tar -xf arduino* -C /opt/
+sudo rm arduino*.tar.xz
 cd /opt/arduino*
 sh ./install.sh
 
--- install_processing.sh
+++ install_processing.sh
@@ -7,13 +7,13 @@ export DISPLAY=:0.0
 
 
 sudo wget http://download.processing.org/processing-3.4-linux64.tgz
-sudo tar -xzf processing-3.4-linux64.tgz -C /opt/
+sudo tar -xzf processing-*-linux64.tgz -C /opt/
 
-sudo rm processing-3.4-linux64.tgz
+sudo rm processing-*-linux64.tgz
 
-sudo su -c "ln -s /opt/processing-3.4/processing /usr/local/bin/processing"
+sudo su -c "ln -s /opt/processing-*/processing /usr/local/bin/processing"
 
-sudo su -c "ln -s /opt/processing-3.4/processing-java /usr/local/bin/processing-java"
+sudo su -c "ln -s /opt/processing-*/processing-java /usr/local/bin/processing-java"
 
 # see http://www.artsnova.com/processing/installing-processing-ubuntu-linux-tutorial.html
 # and https://forum.processing.org/two/discussion/1523/shortcut-for-processing-in-ubuntu-13-10-and-unity
