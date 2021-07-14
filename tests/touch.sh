af3d592479f3a07994d52bc0355d14a433679190
Jens Weber <software@jewe.net>
2021-07-14 11:24:37 +0200

    ...

--- tests/touch.sh
+++ tests/touch.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
+x1=0
+x2=1920
+y1=0
+y2=1080
+
+# next img
+ax=3570
+ay=1100
+
+# back
+bx=160
+by=1910
+
 function random_number {
     # we take two parameters: from and to, inclusive
     # we return random number within this range
     echo $(($1 + RANDOM % $2))
 }
 
+function click {
+  # https://wiki.ubuntuusers.de/xdotool/
+  xdotool mousemove $1 $2 click 1 &
+  sleep $3
+}
 
 export DISPLAY=:0.0
 echo "sending touch events to screen"
 
-x1=0
-x2=1920
-y1=0
-y2=1080
 
 while true; do
+  # random
   x=$(random_number x1 x2)
   y=$(random_number y1 y2)
-  # https://wiki.ubuntuusers.de/xdotool/
-  xdotool mousemove $x $y click 1 &
-  sleep 1
+  $(click x y 10)
+
+
+  $(click bx by 1)
+  $(click bx by 1)
+
+  $(click ax ay 10)
+  echo "---"
 done
