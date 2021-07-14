5d2711174d77d4e0abf95efa0109d6d455efc8a9
Jens Weber <software@jewe.net>
2021-07-14 11:27:27 +0200

    ...

---
+++ tests/touch_02.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+x1=0
+x2=1920
+y1=0
+y2=1080
+
+
+
+function random_number {
+    # we take two parameters: from and to, inclusive
+    # we return random number within this range
+    echo $(($1 + RANDOM % $2))
+}
+
+
+
+export DISPLAY=:0.0
+echo "sending touch events to screen"
+
+
+while true; do
+  # random
+  x=$(random_number x1 x2)
+  y=$(random_number y1 y2)
+  xdotool mousemove x y click 1 &
+  sleep 5
+
+  echo "---"
+done
